41d0bae6484efc95f8d5f68c4ee6bd96405c85f4
Yanitza de la Caridad Gutierrez Acea <yanitza.guti93@gmail.com>
2025-09-09 15:06:33 -0600

    Rename field alimentos on registrocomida to foods

--- App_Diabetes_Django/nutricion/models.py
+++ App_Diabetes_Django/nutricion/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 class RegistroComida(models.Model):
     """
     Modelo que representa un registro diario de comidas para un usuario.
-    El campo alimentos usa JSONField para permitir flexibilidad en el contenido.
+    El campo foods usa JSONField para permitir flexibilidad en el contenido.
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comidas")
     fecha = models.DateField(auto_now_add=True)
-    alimentos = models.JSONField()  # Ejemplo: {"desayuno": [...], "almuerzo": [...]}
+    foods = models.JSONField()  # Ejemplo: {"desayuno": [...], "almuerzo": [...]}
 
     def __str__(self):
         return f"Comida de {self.user.username} - {self.fecha}"
