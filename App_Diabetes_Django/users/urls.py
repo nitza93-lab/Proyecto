620d66631bc7596f48949700060da645c358796c
Yanitza de la Caridad Gutierrez Acea <yanitza.guti93@gmail.com>
2026-04-20 13:31:23 -0600

    change_login

--- App_Diabetes_Django/users/urls.py
+++ App_Diabetes_Django/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView, LoginUserView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register_user'),
+    path('login/', LoginUserView.as_view(), name='login_user'),
 ]
--- App_Diabetes_Django/users/views.py
+++ App_Diabetes_Django/users/views.py
@@ -2,6 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserRegisterSerializer
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
 
 class RegisterUserView(APIView):
     def post(self, request):
@@ -11,3 +14,25 @@ class RegisterUserView(APIView):
             return Response({'message': 'Usuario creado correctamente'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class LoginUserView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        # Buscar usuario por email
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            # Validar contraseña
+            if user.check_password(password):
+                return Response(
+                    {"message": "Login exitoso"},
+                    status=status.HTTP_200_OK
+                )
+
+        return Response(
+            {"error": "Credenciales incorrectas"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+        print("Login recibido:", email)
